3df9cd1c9ffe7df0b0d047df6feaf5b9e7eefec8
banchiec <icaro_19@hotmail.com>
2022-03-23 00:41:04 +0100

    Push historial

---
+++ .history/routes/upload.routes_20220322224325.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const uploader = require('../config/upload.config')
+
+router.post('/image', uploader.single("photo"), (req, res) => {
+
+    if (!req.file) {
+        res.status(500).json({ code: 500, message: 'Error loading the file' })
+        return
+    }
+    console.log(req.body)
+    res.json({ cloudinary_url: req.file.path })
+
+})
+router.post('/profile', uploader.single("photo_profile"), (req, res) => {
+
+    if (!req.file) {
+        res.status(500).json({ code: 500, message: 'Error loading the file' })
+        return
+    }
+    console.log(req.body)
+    res.json({ cloudinary_url: req.file.path })
+
+
+})
+
+
+module.exports = router
